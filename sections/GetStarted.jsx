18d90ed0d1ce4526f9116c013367a48640644fbb
HafeefaPC <hafeefapc2003@gmail.com>
2023-09-07 18:46:34 +0530

    explore

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,4 +1,12 @@
 'use client';
+import { useState } from 'react';
+import {motion}  from 'framer-motion';
+import { ExploreCard,TitleText, TypingText }from '../components';
+
+import styles from '../styles';
+import {exploreWorlds } from '../constants';
+import { staggerContainer} from '../utils/motion';
+
 
 const GetStarted = () => (
   <section>
